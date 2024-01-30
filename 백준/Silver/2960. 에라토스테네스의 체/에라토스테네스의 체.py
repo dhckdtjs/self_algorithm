6eee471d02734787392ed965df40aec6e2c1d063
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-01-30 21:07:20 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -1,17 +1,17 @@
 n,m = list(map(int,input().split()))
 lst = []
 for i in range(2,n+1):
-    lst.append(i)
+    lst.append(i)           # 일단 새로운 리스트에 나열
 
-new_list = []
+new_list = []               # 빈 리스트 선언 후에 이중 for문을 통해 빼낸 순서로 일단 넣기
 for j in range(2,n+1):
     for k in lst:
         if k % j == 0:
             new_list.append(k)
 
-jjin_new_list = []
+jjin_new_list = []          # 최종 리스트 선언 후에 중복 되는 값 빼내기
 for l in new_list:
     if l not in jjin_new_list:
         jjin_new_list.append(l)
 
-print(jjin_new_list[m-1])
+print(jjin_new_list[m-1])      # 원하는 값 출력
