98e8dc589bf457dbb9f1ac02ea223eaaa3359357
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-02-04 15:11:38 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,7 +1,10 @@
+# 풍선 터뜨리기
+# 풍선 원형, 돌리면서 터뜨리고 그 인덱스 출력
 from collections import deque
 N = int(input())
 
 ballon = deque(enumerate(map(int,input().split())))
+# enumerate : 순서가 있는 자료형을 입력으로 받았을 때, 인덱스와 값을 포함하여 리턴
 index_list = []
 for _ in range(N):
     index,paper = ballon.popleft()
