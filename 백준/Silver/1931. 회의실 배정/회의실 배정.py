b3a8117b9a44aa58fefa0a4f1448c8c1c05eb148
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-02-06 08:21:39 +0900

    [Silver I] Title: 회의실 배정, Time: 4044 ms, Memory: 47180 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -2,14 +2,15 @@ n = int(input())
 meeting_list = []
 for _ in range(n):
     S,E = map(int,input().split())
-    meeting_list.append([S,E])
-tmp = sorted(meeting_list,key = lambda x:x[0])
-sort_meeting = sorted(tmp,key = lambda x:x[1])
-start_meeting = sort_meeting[0][0]
-end_meeting = sort_meeting[0][1]
+    meeting_list.append((S,E))
+tmp = sorted(meeting_list,key=lambda x:x[0])
+sort_meeting = sorted(tmp,key=lambda x:x[1])
+first_s = sort_meeting[0][0]
+first_e = sort_meeting[0][1]
 cnt = 1
 for start,end in sort_meeting[1:]:
-    if start>=end_meeting:
+    if first_e<=start:
         cnt+=1
-        end_meeting = end
+        first_s = start
+        first_e = end
 print(cnt)
