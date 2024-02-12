4c913c43c8aa99cd1b8f93849911f9b776df8a5d
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-02-12 12:26:23 +0900

    [Bronze III] Title: 369, Time: 232 ms, Memory: 110432 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,16 +1,21 @@
 # 369
 # 3.6,9가 나오면 박수 횟수 +1
 # 총 박수 횟수 계산
+import sys
+input = sys.stdin.readline
 n = int(input())
 result = 1
 cnt = 0
 while result<=n:
-    for k in str(result):
+    check = str(result)
+    for k in check:
         if k == '3':
             cnt+=1
         if k == '6':
             cnt+=1
         if k == '9':
             cnt+=1
+    if result == n:
+        break
     result+=1
 print(cnt)
