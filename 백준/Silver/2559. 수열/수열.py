901c549dd9c208c4e48c1a2429012cbe685b0ff0
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-02-13 21:42:38 +0900

    [Silver III] Title: 수열, Time: 80 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,12 +1,14 @@
 # 수열
-# 연속되는 값 더한 값 중 최대값 구하기
 import sys
 input = sys.stdin.readline
-N, K = map(int,input().split())
-temp_list = list(map(int,input().split()))
-Sum_list = []
-Sum_list.append(sum(temp_list[:K]))
+N, K = map(int,input().rstrip('\n').split())
+temp_list = list(map(int,input().rstrip('\n').split()))
+Sum = 0
+for k in range(K):
+    Sum+=temp_list[k]
+Max_Sum = Sum
 for i in range(N-K):
-    result = (Sum_list[i]-temp_list[i]+temp_list[i+K])
-    Sum_list.append(result)
-print(max(Sum_list))
+    Sum= Sum-temp_list[i]+temp_list[i+K]
+    if Max_Sum<Sum:
+        Max_Sum= Sum
+print(Max_Sum)
