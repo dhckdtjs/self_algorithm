cbdc062f8c525ac7b77eee7e273bbb159496fe28
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-02-25 23:53:53 +0900

    [Silver I] Title: 트리 순회, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,32 @@
+# 트리 순회
+n = int(input())
+tree = {}
+for i in range(n):
+    root,left,right = map(str,input().split())
+    tree[root] = [left,right]
+
+def pre_order(root):
+    if root !='.':
+        print(root,end='')
+        pre_order(tree[root][0])
+        pre_order(tree[root][1])
+def in_order(root):
+    if root !='.':
+        in_order(tree[root][0])
+        print(root,end='')
+        in_order(tree[root][1])
+
+def post_order(root):
+    if root !='.':
+        post_order(tree[root][0])
+        post_order(tree[root][1])
+        print(root,end='')
+        
+pre_order('A')
+print()
+in_order('A')
+print()
+post_order('A')
+
+        
+
