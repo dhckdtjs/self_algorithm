498f6a0fe553d760c35556d02a81bbc7d5d34f6b
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-03-01 20:39:44 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,16 @@
+# 블랙잭
 N,M = map(int,input().split())
-card_list = list(map(int,input().split()))
-Max_result = 0
-for i in range(len(card_list)):
-    for j in range(i+1,len(card_list)):
-        for k in range(j+1,len(card_list)):
-            if card_list[i]+card_list[j]+card_list[k] == M:
-                Max_result = M
-            elif card_list[i]+card_list[j]+card_list[k] <M:
-                result = card_list[i]+card_list[j]+card_list[k]
-                if Max_result<result:
-                    Max_result = result
 
-print(Max_result)
+card_list = list(map(int,input().split()))
+card_list.sort()
+s = 0
+Max_res = 0
+for i in range(N-2):
+    for j in range(i+1,N-1):
+        for l in range(j+1,N):
+            s=card_list[i]+card_list[j]+card_list[l]
+            if Max_res<s<=M:
+                Max_res = s
+                break
+print(Max_res)
+            
