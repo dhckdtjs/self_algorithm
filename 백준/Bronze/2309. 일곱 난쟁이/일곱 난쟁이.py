3251cc6e9af505dcf37b0b319e3ec1329d44ee48
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-03-01 20:45:52 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,44 +1,34 @@
-# 일곱 난쟁이
-# 9명이 주어지고 합이 100인 7명 찾기 후 오름차순 정렬
-# 안되는 경우 없음
-def selection_sort(arr,n):
-    for i in range(n-1):
-        min_idx = i
-        for j in range(i+1,n):
-            if arr[min_idx]>arr[j]:
-                min_idx = j
-        arr[i],arr[min_idx] = arr[min_idx],arr[i]
-    return arr
-
+little_list = []
 
 def powerset(idx):
     s = 0
     global result
+    res = []
     if idx == N:
-        res = []
-        for k in range(N):
-            if c[k]:
-                s+=little_list[k]
-                res.append(little_list[k])
-            if s>100:
-                break
-        if s == 100 and len(res) == 7:
+        for i in range(N):
+            if c[i]:
+                s+=little_list[i]
+                res.append(little_list[i])
+                if s>100:
+                    return
+        if len(res) == 7 and s == 100:
             result = res
         return
+
     c[idx] = 0
     powerset(idx+1)
     c[idx] = 1
     powerset(idx+1)
-            
 
 
-little_list = []
-for p in range(9):
-    p = int(input())
-    little_list.append(p)
-selection_sort(little_list,9)   # 정렬
-N = 9
-c=[0]*N
+
+for _ in range(9):
+    little = int(input())
+    little_list.append(little)
+little_list.sort()
+N = len(little_list)
+c = [0]*N
+result = []
 powerset(0)
-for person in result:
-    print(person)
+for k in result:
+    print(k)
