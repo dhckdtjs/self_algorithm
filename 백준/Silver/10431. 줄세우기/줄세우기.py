42236e76d014a3d4fd83513b788804fe61a28093
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-03-02 11:51:43 +0900

    [Silver V] Title: 줄세우기, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -3,9 +3,8 @@ n = 20
 for tc in range(1,T+1):
     test_case, *child = list(map(int,input().split()))
     cnt = 0
-    for i in range(n-1,-1,-1):
+    for i in range(1,20):
         for j in range(i):
-            if child[j]>child[j+1]:
-                child[j],child[j+1] = child[j+1],child[j]
+            if child[i]<child[j]:
                 cnt+=1
     print(f'{test_case} {cnt}')
