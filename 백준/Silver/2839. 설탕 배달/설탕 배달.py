bb60724c051c293d212b80b332d52664322b9946
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-03-02 19:43:13 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,21 +1,10 @@
 # 설탕배달
-n = int(input())
-sugar = [5,3]
-i = 0
-j = 0
-res = []
-while 5*i<=n:
-    for j in range((n//3)+1):
-        if 5*i+3*j == n:
-            res.append([i,j])
-    i+=1
 
-min_v = 5000
-if len(res) == 0:
-    print(-1)
+n = int(input())
+for cnt_5 in range(n//5,-1,-1):
+    cnt_3 = (n-(5*cnt_5))//3
+    if cnt_5*5+cnt_3*3 == n:
+        print(cnt_5+cnt_3)
+        break
 else:
-    for k in range(len(res)):
-        result = res[k][0]+res[k][1]
-        if min_v>result:
-            min_v = result
-    print(min_v)
+    print(-1)
