26cfc80f1c4e87f572a7ddd1a7d7f4f7ae361a3e
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-03-02 11:19:19 +0900

    [Bronze II] Title: 줄 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -1,9 +1,6 @@
-# 줄 세우기 
 n = int(input())
-num_list = list(map(int,input().split())) # 0 1 1 3 2  
-# line_list = [i for i in range(n)]       # 1 2 3 4 5
+num_list = list(enumerate(map(int,input().split())))
 line_list = []
-line_list.append(1)
-for i in range(2,n+1):
-    line_list.insert((i-1)-num_list[i-1],i)
+for i in range(n):
+    line_list.insert(i-num_list[i][1],num_list[i][0]+1)
 print(*line_list)
