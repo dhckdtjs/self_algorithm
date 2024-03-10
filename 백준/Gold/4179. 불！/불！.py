eef570b9d5ecd9de4456a4b04d50bc9267cffa53
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-03-10 19:42:06 +0900

    [Gold IV] Title: 불!, Time: 1488 ms, Memory: 71544 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,50 +1,54 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
+R,C = map(int,input().split())
+maze = [list(input().rstrip()) for _ in range(R)]
+fire = [[0]*C for _ in range(R)]
+human = [[0]*C for _ in range(R)]
 
-R, C = map(int, input().split())
-maze = [list(input().strip()) for _ in range(R)]
-human = [[0] * C for _ in range(R)]
-fire = [[0] * C for _ in range(R)]
+dr = [0,0,-1,1]
+dc = [1,-1,0,0]
 
-dr = [0, 0, -1, 1]
-dc = [1, -1, 0, 0]
-
-q_f = deque()
-q_j = deque()
-
-for row in range(R):
-    for col in range(C):
-        if maze[row][col] == 'F':
-            q_f.append((row, col))
-            fire[row][col] = 1
-        elif maze[row][col] == 'J':
-            q_j.append((row, col))
-            human[row][col] = 1
-
-def bfs_fire():
+def bfs_f():
+    q_f = deque()
+    for r in range(R):
+        for c in range(C):
+            if maze[r][c] == 'F':
+                q_f.append((r,c))
+                fire[r][c] = 1
     while q_f:
-        r, c = q_f.popleft()
+        row,col = q_f.popleft()
         for k in range(4):
-            nr, nc = r + dr[k], c + dc[k]
-            if 0 <= nr < R and 0 <= nc < C and maze[nr][nc] != '#' and fire[nr][nc] == 0:
-                fire[nr][nc] = fire[r][c] + 1
-                q_f.append((nr, nc))
+            nr = row+dr[k]
+            nc = col+dc[k]
+            if 0<=nr<R and 0<=nc<C:
+                if maze[nr][nc] !='#' and fire[nr][nc] ==0:
+                    q_f.append((nr,nc))
+                    fire[nr][nc]=fire[row][col]+1
 
-def bfs_human():
-    while q_j:
-        r, c = q_j.popleft()
-        if r == 0 or r == R-1 or c == 0 or c == C-1:  # 탈출 조건
-            print(human[r][c])
-            return
-        for k in range(4):
-            nr, nc = r + dr[k], c + dc[k]
-            if 0 <= nr < R and 0 <= nc < C and maze[nr][nc] != '#' and human[nr][nc] == 0:
-                if fire[nr][nc] == 0 or human[r][c] + 1 < fire[nr][nc]:
-                    human[nr][nc] = human[r][c] + 1
-                    q_j.append((nr, nc))
-    print("IMPOSSIBLE")
+def bfs_h():
+    q_h = deque()
+    for r in range(R):
+        for c in range(C):
+            if maze[r][c] == 'J':
+                q_h.append((r,c))
+                human[r][c] =1
+    while q_h:
+        row,col = q_h.popleft()
+        if row == R-1 or row == 0 or col ==C-1 or col == 0:
+            return human[row][col]
+        else:
+            for k in range(4):
+                nr = row+dr[k]
+                nc = col+dc[k]
+                if 0<=nr<R and 0<=nc<C:
+                    if maze[nr][nc] != '#' and human[nr][nc] == 0:
+                        if fire[nr][nc] == 0 or human[row][col]+1<fire[nr][nc]:
+                            q_h.append((nr,nc))
+                            human[nr][nc] = human[row][col]+1
+    return 'IMPOSSIBLE'
 
-bfs_fire()
-bfs_human()
+
+bfs_f()
+res =bfs_h()
+print(res)
