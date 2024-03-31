b29ef3f5cabdd4371d9b494c89bc4dce499cab86
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-03-31 18:09:25 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,17 +1,12 @@
-# RGB 거리
 import sys
 input = sys.stdin.readline
 n = int(input())
-house = [list(map(int,input().strip().split())) for _ in range(n)]
+arr = [list(map(int,input().strip().split())) for _ in range(n)]
 dp = [[0]*3 for _ in range(n)]
-dp[0] = house[0]
-
+dp[0] = arr[0]
+# print(dp)
 for i in range(1,n):
-    dp[i][0] = min(dp[i-1][1],dp[i-1][2])+house[i][0]
-    dp[i][1] = min(dp[i-1][0],dp[i-1][2])+house[i][1]
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + house[i][2]
-Min = float('inf')
-for k in dp[-1]:
-    if Min>k:
-        Min = k
-print(Min)
+    dp[i][0] = min(dp[i-1][1],dp[i-1][2])+arr[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2]
+print(min(dp[-1]))
