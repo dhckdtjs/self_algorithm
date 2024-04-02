2983d2ea7c6e01576803e72150fee02664f646d1
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-04-02 20:42:02 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -4,17 +4,10 @@ input = sys.stdin.readline
 n = int(input())
 num = list(map(int,input().strip().split()))
 dp = [0]*n
-
-# 각 요소에 대해 dp[i]를 num[i]로 초기화합니다.
 for i in range(n):
     dp[i] = num[i]
-
-# i번째 요소에 대해 i보다 앞에 있는 요소들 중 num[i]보다 작은 것들을 찾아
-# 그 위치의 dp 값에 num[i]를 더한 값으로 dp[i]를 업데이트합니다.
-for i in range(1,n):
+for i in range(n):
     for j in range(i):
-        if num[i] > num[j]:
-            dp[i] = max(dp[i], num[i] + dp[j])
-
-# 가장 큰 증가하는 부분 수열의 합을 출력합니다.
+        if num[i]>num[j]:
+            dp[i] = max(dp[i],dp[j]+num[i])
 print(max(dp))
