c08aec0fcaf2f0879cc066f5cb32ad0f49fafb29
dhckdtjs <156289544+dhckdtjs@users.noreply.github.com>
2024-04-04 22:00:27 +0900

    [Silver V] Title: 지뢰 찾기, Time: 3012 ms, Memory: 46180 KB -BaekjoonHub

--- 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.py
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.py
@@ -8,23 +8,27 @@ dc = [1,-1,0,0,-1,1,-1,1]
 
 mine = [list(input().strip()) for _ in range(n)]
 draw = [[0]*n for _ in range(n)]
+
+def mining(r,c):
+    temp = int(mine[r][c])
+    for k in range(8):
+        nr = r + dr[k]
+        nc = c + dc[k]
+        if 0 <= nr < n and 0 <= nc < n:
+            if draw[nr][nc] == '*' or draw[nr][nc] == 'M':
+                continue
+            elif draw[nr][nc] == 0 and mine[nr][nc] == '.':
+                draw[nr][nc] = temp
+            elif draw[nr][nc] >= 0 and mine[nr][nc] == '.':
+                draw[nr][nc] = draw[nr][nc] + temp
+                if draw[nr][nc] >= 10:
+                    draw[nr][nc] = 'M'
 for r in range(n):
     for c in range(n):
         if mine[r][c] !='.':
             draw[r][c] = '*'
-            temp = int(mine[r][c])
-            for k in range(8):
-                nr = r+dr[k]
-                nc = c+dc[k]
-                if 0<=nr<n and 0<=nc<n:
-                    if draw[nr][nc] == '*' or draw[nr][nc] =='M':
-                        continue
-                    elif draw[nr][nc] ==0 and mine[nr][nc] =='.':
-                        draw[nr][nc] = temp
-                    elif draw[nr][nc] >=0 and mine[nr][nc] == '.':
-                        draw[nr][nc]=draw[nr][nc]+temp
-                        if draw[nr][nc]>=10:
-                            draw[nr][nc] = 'M'
+            mining(r,c)
+
 for l in draw:
     k = ''
     for j in l:
